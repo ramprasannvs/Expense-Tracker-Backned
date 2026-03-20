2dfdf0d8ffab98cc99999fb4c1c4e3d52446de52
Ram Prasann <106461823+ramprasannvs@users.noreply.github.com>
2026-03-20 11:16:42 +0530

    U

--- Models/db.js
+++ Models/db.js
@@ -2,9 +2,14 @@ const mongoose = require('mongoose');
 
 const mongo_url = process.env.MONGO_CONN;
 
-mongoose.connect(mongo_url)
-    .then(() => {
-        console.log('MongoDB Connected...');
-    }).catch((err) => {
-        console.log('MongoDB Connection Error: ', err);
-    })
+if (!mongo_url) {
+    console.log("❌ MONGO_CONN is missing");
+} else {
+    mongoose.connect(mongo_url)
+        .then(() => {
+            console.log('MongoDB Connected...');
+        })
+        .catch((err) => {
+            console.log('MongoDB Connection Error: ', err);
+        });
+}
