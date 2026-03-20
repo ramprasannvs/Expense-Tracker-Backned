121193acb3f74b5ea156f13964e7256bf883f672
Ram Prasann <106461823+ramprasannvs@users.noreply.github.com>
2026-03-20 11:10:42 +0530

    U

--- index.js
+++ index.js
@@ -7,11 +7,8 @@ require("dotenv").config();
 require("./Models/db");
 
 const AuthRouter = require("./Routes/AuthRouter");
-// const ProductRouter = require("./Routes/ProductRouter");
 const ExpenseRouter = require("./Routes/ExpenseRouter");
 const ensureAuthenticated = require("./Middlewares/Auth");
-
-const PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -21,9 +18,6 @@ app.get("/ping", (req, res) => {
 });
 
 app.use("/auth", AuthRouter);
-// app.use("/products", ProductRouter);
 app.use("/expenses", ensureAuthenticated, ExpenseRouter);
 
-app.listen(PORT, () => {
-    console.log(`Server is running on ${PORT}`);
-});
+module.exports = app;
