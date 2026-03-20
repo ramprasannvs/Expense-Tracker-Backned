91cab1abc5038c804e1e24499ea74d877dde60bc
Ram Prasann <106461823+ramprasannvs@users.noreply.github.com>
2026-03-20 11:30:33 +0530

    U

--- Models/db.js
+++ Models/db.js
@@ -1,15 +1,12 @@
 const mongoose = require('mongoose');
 
-const mongo_url = process.env.MONGO_CONN;
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_CONN);
+        console.log("MongoDB Connected");
+    } catch (err) {
+        console.log("DB Error:", err);
+    }
+};
 
-if (!mongo_url) {
-    console.log("❌ MONGO_CONN is missing");
-} else {
-    mongoose.connect(mongo_url)
-        .then(() => {
-            console.log('MongoDB Connected...');
-        })
-        .catch((err) => {
-            console.log('MongoDB Connection Error: ', err);
-        });
-}
+module.exports = connectDB;
--- index.js
+++ index.js
@@ -1,25 +1,30 @@
 const express = require("express");
 const app = express();
-const bodyParser = require("body-parser");
 const cors = require("cors");
 
 require("dotenv").config();
 
+// DB connect
 const connectDB = require("./Models/db");
 connectDB();
 
+// Routes
 const AuthRouter = require("./Routes/AuthRouter");
 const ExpenseRouter = require("./Routes/ExpenseRouter");
 const ensureAuthenticated = require("./Middlewares/Auth");
 
-app.use(bodyParser.json());
+// Middleware
+app.use(express.json());
 app.use(cors());
 
+// Test route
 app.get("/ping", (req, res) => {
     res.send("PONG");
 });
 
+// Routes
 app.use("/auth", AuthRouter);
 app.use("/expenses", ensureAuthenticated, ExpenseRouter);
 
+// Export for Vercel
 module.exports = app;
